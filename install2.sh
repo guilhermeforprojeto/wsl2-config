0918cbf826a1989b8d74afd6c11966b57368bd46
Guilherme Santana Santos <guylhermesantos@hotmail.com>
2023-03-21 18:21:18 -0300

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,8 @@
+wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.bashrc" SHELL="$(which bash)" bash -
+
+source /home/guilherme/.bashrc 
+source /home/guilherme/.zshrc
+exit 
+
+npm install -g pnpm
+
